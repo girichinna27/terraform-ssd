a44d13661d46228bf6dd75e80a5ef725e9013407
giribabuthanikonda <52537095+girichinna27@users.noreply.github.com>
2023-11-17 00:17:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,3 +10,11 @@ resource "aws_instance" "ubuntuos" {
     Name = "UbuntuOS"
   }
 }
+resource "aws_s3_bucket" "dev_bucket" {
+  bucket = "my-tf-test-bucket"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
