057babe34e81ae9780b5f43f67d46f152e7cf8ec
giribabuthanikonda <52537095+girichinna27@users.noreply.github.com>
2023-11-16 22:26:46 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "ubuntuos" {
+  ami           = "ami-0fc5d935ebf8bc3bc"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "UbuntuOS"
+  }
+}
